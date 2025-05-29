8740ca25e711a60f56bd2c6aeacc09d2b68dd59a
Ale Coss <alexacoss99@gmail.com>
2025-05-29 17:48:48 -0600

    Lección 7, clase

---
+++ Lección 7/Clase/api/db.js
@@ -0,0 +1,32 @@
+/* Este archivo ayuda a comunicarme con la base de datos, crear las tablas en caso de que no existan y el archivo donde va a vivir la base de datos */
+/* Conectarse a la sabe de datos sqlite y ejecuto consultas previas a todo */
+
+/* Importamos SQLIte3 */
+const sqlite3 = require('sqlite3').verbose();
+/* Creamos instancia de base de datos */
+const db = new sqlite3.Database('./data.sqlite', (err) => { // Al crear también crea el archivo "data.sqlite"
+    if (err) throw err;
+});
+
+// corremos la creación de las tablas necesarias (en caso que no existan)
+db.serialize(() => {
+    db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      email TEXT UNIQUE NOT NULL,
+      password TEXT NOT NULL,
+      phone TEXT
+    );
+  `);
+    db.run(`
+    CREATE TABLE IF NOT EXISTS todos (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      task TEXT NOT NULL,
+      dueDate TEXT NOT NULL,
+      done INTEGER NOT NULL DEFAULT 0
+    );
+  `);
+});
+
+module.exports = db;
